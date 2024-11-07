efe02ad31a81b53f1134c08ac618d395e5c472b4
Rakesh-ganiger <ganigerrakesh1709@gmail.com>
2024-11-07 19:34:43 +0530

    Shallow copy and deep copy

--- blockMainThread.js
+++ blockMainThread.js
@@ -125,55 +125,7 @@ const cart=["shirts","Pants","Kurtas"]
 //   console.log(sayHello.call(obj))
 
 
-  let obj1={
-    firstName:"Rakesh",
-    lastname:"Ganiger",
-    
-  }
-  let fullname=function(hometown,state){
-    console.log(this.firstName + " " + this.lastname +" from " + hometown + state);
-}
 
-  let obj2={
-    firstName:"Rahul",
-    lastname:"Ganiger"
-}
-Function.prototype.myCall=function (context={}, ...args){
-    if(typeof this !== "function"){
-        throw new Error("Its not collable")
-    }
-    context.fn=this;
-    context.fn(...args)
-    
-};
-
-Function.prototype.myBind=function (context={}, ...args){
-    if(typeof this !== "function"){
-        throw new Error("Its not collable")
-    }
-    context.fn=this;
-    return function(...newArgs){
-        return context.fn(...args, ...newArgs)
-    }
-    
-    
-};
-
-
-Function.prototype.myApply=function (context={}, args=[]){
-    if(typeof this !== "function"){
-        throw new Error("Its not collable")
-    }
-    if(!Array.isArray(args)){
-        throw new Error("Not array")
-    }
-    context.fn=this;
-    context.fn(...args)
-    
-};
-
-  const res=fullname.myBind(obj2,"Belagavi")
-  console.log(res("Karnataka"))
 
 
   //currying
@@ -197,11 +149,15 @@ Function.prototype.myApply=function (context={}, args=[]){
 //     return temp;
 // }
 
+
+
+//pollyfill for mao filter reduce
+
 // const nums=[1,2,3,4];
-// const res=nums.myMap((e)=>{
+// const res2=nums.myMap((e)=>{
 //     return e*2;
 // })
-// console.log(res)
+// console.log(res2)
 
 
 // Array.prototype.myFilter=function(cb){
@@ -216,3 +172,124 @@ Function.prototype.myApply=function (context={}, args=[]){
 //     return e>2;
 // })
 // console.log(res1)
+
+
+
+
+// //pollyfill for call bind apply
+// let obj1={
+//     firstName:"Rakesh",
+//     lastname:"Ganiger",
+    
+//   }
+//   let fullname=function(hometown,state){
+//     console.log(this.firstName + " " + this.lastname +" from " + hometown + state);
+// }
+
+//   let obj2={
+//     firstName:"Rahul",
+//     lastname:"Ganiger"
+// }
+// Function.prototype.myCall=function (context={}, ...args){
+//     if(typeof this !== "function"){
+//         throw new Error("Its not collable")
+//     }
+//     context.fn=this;
+//     context.fn(...args)
+    
+// };
+
+// Function.prototype.myBind=function (context={}, ...args){
+//     if(typeof this !== "function"){
+//         throw new Error("Its not collable")
+//     }
+//     context.fn=this;
+//     return function(...newArgs){
+//         return context.fn(...args, ...newArgs)
+//     }
+    
+    
+// };
+
+
+// Function.prototype.myApply=function (context={}, args=[]){
+//     if(typeof this !== "function"){
+//         throw new Error("Its not collable")
+//     }
+//     if(!Array.isArray(args)){
+//         throw new Error("Not array")
+//     }
+//     context.fn=this;
+//     context.fn(...args)
+    
+// };
+
+//   const res=fullname.myBind(obj2,"Belagavi")
+//   console.log(res("Karnataka"))
+
+
+// const nums=[1,2,3,4,5];
+
+// const[first , ,second]=nums;
+// console.log(second)
+
+
+//rest operators
+
+// function add(a,b,c,d,...others){
+//     console.log(others)
+//     return a+b+c+d
+// }
+// console.log(add(1,2,3,4,6,7,9))
+
+
+
+// //spraed operator
+
+// const name=["RAkesh","RAhul","RAjesh"]
+
+// function print(name1,name2,name3){
+//     console.log(name1,name2,name3)
+// }
+// print(...name)
+// print(name[0],name[1],name[2])
+
+
+
+function memo(){
+    const cache={}
+    return function fibo(n){
+        if(n in cache){
+            return cache[n]
+        }
+        else if(n>=1){
+            return n;
+        }
+        else{
+            let res=fibo(n-1)+fibo(n-2);
+            cache[n]=res;
+            return res;
+        } 
+    }
+}
+var x=memo()
+console.log(x(5));
+// Output: 5
+function memo() {
+    const cache = {};
+    return function fiboSeries(n) {
+        if (n in cache) {
+            return cache[n];
+        }
+        if (n === 0) return [0];
+        if (n === 1) return [0, 1];
+        
+        let series = fiboSeries(n - 1);
+        series.push(series[n - 1] + series[n - 2]);
+        cache[n] = series;
+        return series;
+    };
+}
+
+const y = memo();
+console.log(y(5)); // Output: [0, 1, 1, 2, 3, 5]
--- higher-order-function.js
+++ higher-order-function.js
@@ -1,27 +1,61 @@
-const radius=[2,3,4,5]
+//hIGHER ORDER FUNCTIONS
 
-const area=function(radius){
-    return Math.PI * radius * radius;
-};
-const circumFerance=function(radius){
-    return Math.PI * radius *2;
-}
-const calculateValue = function(arr,logic){
-    const output=[];
-    for(let i=0;i<arr.length;i++){
-        output.push(logic(arr[i]))
+
+// const radius=[2,3,4,5]
+
+// const area=function(radius){
+//     return Math.PI * radius * radius;
+// };
+// const circumFerance=function(radius){
+//     return Math.PI * radius *2;
+// }
+// const calculateValue = function(arr,logic){
+//     const output=[];
+//     for(let i=0;i<arr.length;i++){
+//         output.push(logic(arr[i]))
+//     }
+//     return output;
+// }
+// console.log(calculateValue(radius,area))
+// console.log(calculateValue(radius,circumFerance))
+
+// const calculate=function(radius){
+//     const output=[];
+//     for(let i=0;i<radius.length;i++){
+//         output.push(Math.PI * radius[i] * radius[i])
+//     }
+//     return output;
+// }
+
+// console.log(calculate(radius))
+
+
+
+const person={
+    name:"Rakesh",
+    age:23,
+    adress:{
+        city:"Belagavi"
     }
-    return output;
-}
-console.log(calculateValue(radius,area))
-console.log(calculateValue(radius,circumFerance))
 
-const calculate=function(radius){
-    const output=[];
-    for(let i=0;i<radius.length;i++){
-        output.push(Math.PI * radius[i] * radius[i])
-    }
-    return output;
 }
+//shallow copy
+// const person1=Object.assign({},person)
 
-console.log(calculate(radius))
+// const person1 = {...person}
+
+// person1.name="Rahul";
+// person1.adress.city="Karnataka";
+// console.log(person)
+// console.log(person1)
+
+
+//deep copy
+
+const person1=JSON.parse(JSON.stringify(person));
+
+person1.name="Rahul";
+person1.adress.city="Karnataka";
+console.log(person1)
+console.log(person)
+
